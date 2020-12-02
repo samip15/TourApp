8a8c4bf354c70b83c3dcea0ab02564eeabaa2e73
Samip <samipgnyawali15@gmail.com>
2020-12-02 17:23:25 +0545

    first commit

---
+++ android/app/src/main/java/com/samip/tour_app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.samip.tour_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:tour_app/screens/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Travel',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primaryColor: Color(0xFFF3F5F7),
+          accentColor: Color(0xFFD8ECF1),
+          scaffoldBackgroundColor: Color(0xFFF3F5F7)),
+      home: HomeScreen(),
+    );
+  }
+}
